4af511bbcb65486a8811c84080d29b3f4ae38914
ShaamNatheshwar <75720338+ShaamNatheshwar@users.noreply.github.com>
2021-10-11 22:14:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const container = document.getElementById('container');
 
 signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
+	
 });
 
 signInButton.addEventListener('click', () => {
